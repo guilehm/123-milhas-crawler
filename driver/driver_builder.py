a518ee94b7279f24c912f26008d3491d1cb6f24f
guilherme <guile.hm@hotmail.com>
2021-10-26 21:14:47 -0300

    create driver builder

---
+++ driver/driver_builder.py
@@ -0,0 +1,40 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/60.0.3112.50 Safari/537.36'
+DRIVER_NAME = os.getenv('DRIVER_NAME', 'chromedriver')
+CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH')
+GOOGLE_CHROME_BIN = os.getenv('GOOGLE_CHROME_BIN')
+
+
+class Driver:
+
+    @staticmethod
+    def __get_location():
+        return CHROMEDRIVER_PATH or f'{os.getcwd()}/driver/chromedriver/{DRIVER_NAME}'
+
+    def __init__(self, headless=True, user_agent=USER_AGENT):
+        self.location = self.__get_location()
+        self.headless = headless
+        self.user_agent = user_agent
+
+    def _get_options(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1500,900')
+        chrome_options.add_argument('--disable-geolocation')
+        chrome_options.add_argument('--disable-notifications')
+        if self.user_agent:
+            chrome_options.add_argument(f'user-agent={self.user_agent}')
+        if GOOGLE_CHROME_BIN:
+            chrome_options.binary_location = GOOGLE_CHROME_BIN
+        if self.headless:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+        return chrome_options
+
+    def get_driver(self):
+        return webdriver.Chrome(self.location, options=self._get_options())
